6539699c325615d0d249360f7b68bbbba8865e19
ikinopiyanto <30852760+ikinopiyanto@users.noreply.github.com>
2017-08-09 10:34:52 +0700

    Add files via upload

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Survey App built with Laravel :: Khophi's Dev</title>
+        {!! MaterializeCSS::include_css() !!}
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+ 
+    <body>
+      <div class="container">
+          <!-- TOP MENU -->
+          <div class="row" style="padding-top:10px;">
+              <div class="center-align">
+                <a class="btn blue waves-effect waves-light lighten-1 white-text" href="/"> Home </a>
+                  @if(Auth::check())
+                    <a class="btn-flat waves-effect waves-light darken-1 white black-text" href="/logout"> Logout </a>
+                    <a class="btn-flat disabled" href="#" style="text-transform:none;">{{ Auth::user()->email }}</a>
+                  @else
+                    <a class="btn-flat waves-effect waves-light darken-1 white black-text" href="/login"> Login </a>
+                    <a class="btn-flat waves-effect waves-light darken-1 white black-text" href="/register"> Register </a>
+                  @endif
+              </div>
+          </div>
+         <!-- End TOP MENU -->
+         
+         <!-- BODY OF PAGE -->
+          <div class="row">
+              <div class="col s12 m10 offset-m1 l8 offset-l2" style="margin-top:10px;">
+                @yield('content')
+              </div>
+          </div>
+         <!-- End BODY OF PAGE -->
+      </div>
+    </body>
+    <script src="{{ URL::asset('jquery.min.js') }}"></script>
+    {!! MaterializeCSS::include_js() !!}
+    <script src="{{ URL::asset('init.js') }}"></script>
+ 
+</html>
